43c7159297c1b520375c0a2f0902316b3663a676
gustavogg23 <ggutierrez.7080@unimar.edu.ve>
2024-02-14 19:13:14 -0400

    Creando clase Libro

---
+++ Libro.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaUnimar
+{
+    public class Libro
+    {
+        // Atributos
+        private string titulo;
+        private string autor;
+        private string categoria;
+
+        // Método Constructor
+        public Libro(string titulo, string autor, string categoria)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.categoria = categoria;
+        }
+
+        // Métodos Getters
+        public string getTitulo()
+        {
+            return titulo;
+        }
+
+        public string getAutor()
+        {
+            return autor;
+        }
+
+        public string getCategoria()
+        {
+            return categoria;
+        }
+    }
+}
