49a2d4c67b48907e2faaaaae8df6995a1059ee00
gustavogg23 <ggutierrez.7080@unimar.edu.ve>
2024-02-14 20:36:04 -0400

    Agregando Clase NodoEs, para usar en la lista de Estudiantes

---
+++ NodoEs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaUnimar
+{
+    public class NodoEs
+    {
+        // Atributos de la clase Nodo Estudiantes
+        private Estudiante valor;
+        private NodoEs siguiente;
+
+        // Método Constructor
+        public NodoEs(Estudiante valor)
+        {
+            this.valor = valor;
+            this.siguiente = null;
+        }
+
+        // Métodos Getters
+        public Estudiante getValor()
+        {
+            return valor;
+        }
+
+        public NodoEs getSiguiente()
+        {
+            return siguiente;
+        }
+
+        // Métodos Setters
+        public void setValor(Estudiante valor)
+        {
+            this.valor = valor;
+        }
+
+        public void setSiguiente(NodoEs siguiente)
+        {
+            this.siguiente = siguiente;
+        }
+    }
+}
