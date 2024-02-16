9f50cb24233210951b8f469e993f1303e6493d75
gustavogg23 <ggutierrez.7080@unimar.edu.ve>
2024-02-16 17:03:20 -0400

    Agregando validación de caja de texto de cédula para registro de préstamo.

--- frmRegistrarPrestamo.Designer.cs
+++ frmRegistrarPrestamo.Designer.cs
@@ -28,61 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lblRegistroPrestamo = new System.Windows.Forms.Label();
+            this.lblIngresoCedula = new System.Windows.Forms.Label();
             this.txtCedula = new System.Windows.Forms.TextBox();
             this.lblCedula = new System.Windows.Forms.Label();
-            this.btnControlUsuario = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // label3
+            // lblRegistroPrestamo
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Consolas", 28F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(6, 15);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(440, 45);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Registro De Prestamo";
+            this.lblRegistroPrestamo.AutoSize = true;
+            this.lblRegistroPrestamo.Font = new System.Drawing.Font("Consolas", 28F, System.Drawing.FontStyle.Bold);
+            this.lblRegistroPrestamo.Location = new System.Drawing.Point(6, 15);
+            this.lblRegistroPrestamo.Name = "lblRegistroPrestamo";
+            this.lblRegistroPrestamo.Size = new System.Drawing.Size(440, 45);
+            this.lblRegistroPrestamo.TabIndex = 0;
+            this.lblRegistroPrestamo.Text = "Registro De Préstamo";
             // 
-            // label4
+            // lblIngresoCedula
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(50, 97);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(372, 26);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Ingrese La Cedula Del Usuario ";
+            this.lblIngresoCedula.AutoSize = true;
+            this.lblIngresoCedula.Location = new System.Drawing.Point(50, 97);
+            this.lblIngresoCedula.Name = "lblIngresoCedula";
+            this.lblIngresoCedula.Size = new System.Drawing.Size(372, 26);
+            this.lblIngresoCedula.TabIndex = 1;
+            this.lblIngresoCedula.Text = "Ingrese la Cédula del Usuario ";
             // 
             // txtCedula
             // 
-            this.txtCedula.Location = new System.Drawing.Point(207, 191);
+            this.txtCedula.Location = new System.Drawing.Point(207, 167);
             this.txtCedula.Name = "txtCedula";
             this.txtCedula.Size = new System.Drawing.Size(139, 32);
             this.txtCedula.TabIndex = 7;
+            this.txtCedula.TextChanged += new System.EventHandler(this.txtCedula_TextChanged);
+            this.txtCedula.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCedula_KeyPress);
             // 
             // lblCedula
             // 
             this.lblCedula.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCedula.ForeColor = System.Drawing.Color.White;
-            this.lblCedula.Location = new System.Drawing.Point(95, 187);
+            this.lblCedula.Location = new System.Drawing.Point(95, 163);
             this.lblCedula.Name = "lblCedula";
             this.lblCedula.Size = new System.Drawing.Size(133, 38);
             this.lblCedula.TabIndex = 6;
             this.lblCedula.Text = "Cédula:";
             this.lblCedula.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCedula.Click += new System.EventHandler(this.lblCedula_Click);
             // 
-            // btnControlUsuario
+            // btnBuscar
             // 
-            this.btnControlUsuario.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.btnControlUsuario.Location = new System.Drawing.Point(168, 260);
-            this.btnControlUsuario.Name = "btnControlUsuario";
-            this.btnControlUsuario.Size = new System.Drawing.Size(100, 41);
-            this.btnControlUsuario.TabIndex = 8;
-            this.btnControlUsuario.Text = "Buscar ";
-            this.btnControlUsuario.UseVisualStyleBackColor = false;
-            this.btnControlUsuario.Click += new System.EventHandler(this.btnControlUsuario_Click);
+            this.btnBuscar.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnBuscar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBuscar.Location = new System.Drawing.Point(168, 236);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(100, 41);
+            this.btnBuscar.TabIndex = 8;
+            this.btnBuscar.Text = "Buscar ";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnControlUsuario_Click);
             // 
             // panel1
             // 
@@ -96,14 +100,17 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(72)))), ((int)(((byte)(124)))));
             this.ClientSize = new System.Drawing.Size(1094, 551);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.btnControlUsuario);
+            this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.txtCedula);
             this.Controls.Add(this.lblCedula);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblIngresoCedula);
+            this.Controls.Add(this.lblRegistroPrestamo);
             this.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Bold);
             this.ForeColor = System.Drawing.Color.White;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmRegistrarPrestamo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,11 +120,11 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblRegistroPrestamo;
+        private System.Windows.Forms.Label lblIngresoCedula;
         private System.Windows.Forms.TextBox txtCedula;
         private System.Windows.Forms.Label lblCedula;
-        private System.Windows.Forms.Button btnControlUsuario;
+        private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.Panel panel1;
     }
 }
